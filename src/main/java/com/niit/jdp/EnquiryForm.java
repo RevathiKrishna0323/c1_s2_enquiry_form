757a9b200334bfba1f1ec8beeaabd37a87580756
Revathi G <revathikrishna0323@gmail.com>
2022-08-03 16:04:19 +0530

    Written the code.

--- src/main/java/com/niit/jdp/EnquiryForm.java
+++ src/main/java/com/niit/jdp/EnquiryForm.java
@@ -2,6 +2,24 @@ package com.niit.jdp;
 
 public class EnquiryForm {
     public static void main(String[] args) {
+        String name = "John";
+        long contactNumber = 412312556;
+        String email = "john@gmail.com";
+        int age = 34;
+        char gender = 'M';
+        String occupation = "Manager";
+        double annualSalary = 10_000;
+        String nationality = "Indian";
+        Boolean isMarried = true;
+        System.out.println("name = " + name);
+        System.out.println("contactNumber = " + contactNumber);
+        System.out.println("email = " + email);
+        System.out.println("age = " + age);
+        System.out.println("gender = " + gender);
+        System.out.println("occupation = " + occupation);
+        System.out.println("annualSalary = " + "$" + annualSalary);
+        System.out.println("nationality = " + nationality);
+        System.out.println("isMarried = " + isMarried);
 
     }
 }
